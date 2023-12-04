1004c1a6498bf489b0d8656a01be30e849e0ce87
tiuscia <tiuscia@gmail.com>
2023-12-04 00:38:17 +0000

    added all form dields

--- app/(components)/TicketForm/TicketForm.tsx
+++ app/(components)/TicketForm/TicketForm.tsx
@@ -74,6 +74,75 @@ const TicketForm = () => {
           <option value="Bug">Bug</option>
           <option value="New Feature">New Feature</option>
         </select>
+        <label>Priority</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label>1</label>
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
+        </div>
+        <label>Progress</label>
+        <input
+          type="range"
+          id="progress"
+          name="progress"
+          value={formData.progress as number}
+          min="0"
+          max="100"
+          onChange={handleChange}
+        />
+        <label>Status</label>
+        <select
+          name="status"
+          value={formData.status as string}
+          onChange={handleChange}
+        >
+          <option value="not started">Not Started</option>
+          <option value="started">Started</option>
+          <option value="done">Done</option>
+        </select>
+        <input type="submit" className="btn" value={"Create Ticket"} />
       </form>
     </div>
   )
