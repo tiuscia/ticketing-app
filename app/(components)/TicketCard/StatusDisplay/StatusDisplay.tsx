704ee0e537199cbbac6b5e1f36a2287483976a0b
tiuscia <tiuscia@gmail.com>
2023-12-04 18:00:34 +0000

    added colors to status

--- app/(components)/TicketCard/StatusDisplay/StatusDisplay.tsx
+++ app/(components)/TicketCard/StatusDisplay/StatusDisplay.tsx
@@ -3,8 +3,27 @@ type StatusDisplayProps = {
 }
 
 const StatusDisplay = ({ status }: StatusDisplayProps) => {
+  let color = "bg-slate-700"
+
+  switch (status) {
+    case "not started":
+      color = "bg-green-200"
+      break
+    case "started":
+      color = "bg-yellow-200"
+      break
+    case "done":
+      color = "bg-red-200"
+      break
+    default:
+      color = "bg-slate-700"
+      break
+  }
+
   return (
-    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200">
+    <span
+      className={`${color} inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700`}
+    >
       {status}
     </span>
   )
