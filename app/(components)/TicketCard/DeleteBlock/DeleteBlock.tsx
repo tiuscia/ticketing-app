ae97c48d2c33939c13667f6b4ceac0b4aad11771
tiuscia <tiuscia@gmail.com>
2023-12-06 01:02:00 +0000

    added fetch error

--- app/(components)/TicketCard/DeleteBlock/DeleteBlock.tsx
+++ app/(components)/TicketCard/DeleteBlock/DeleteBlock.tsx
@@ -11,14 +11,26 @@ type DeleteBlockProp = {
 const DeleteBlock = ({ id }: DeleteBlockProp) => {
   const deteleTicket = async (id: string) => {
     const router = useRouter()
+    try {
+      const res: Response = await fetch(
+        `http://localhost:3000/api/Tickets/${id}`,
+        {
+          method: "DELETE",
+        }
+      )
 
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
-      method: "DELETE",
-    })
-
-    // if (res.ok) {
-    //   router.reload()
-    // }
+      if (res.ok) {
+        console.log("DELETE: Promise resolved and HTTP status is successful")
+      } else {
+        // Custom message for failed HTTP codes
+        if (res.status === 404) throw new Error("404, Not found")
+        if (res.status === 500) throw new Error("500, internal server error")
+        // For any other server error
+        if (!res.ok) throw new Error(res.status.toString())
+      }
+    } catch (error) {
+      console.error("Fetch", error)
+    }
   }
 
   return (
